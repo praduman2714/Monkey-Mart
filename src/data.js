3c12292fdcabbb891c23a955f63669f7e24f4519
praduman2714 <pradu14jsr@gmail.com>
2023-05-24 17:08:16 +0530

    Added some more data

--- src/data.js
+++ src/data.js
@@ -32,7 +32,7 @@ const data = [
         value : 'Add To Cart'
     },
     {
-        id: 1,
+        id: 5,
         img : 'https://m.media-amazon.com/images/I/61o7dUNtdsL._UY606_.jpg',
         title : 'Men Half T-Shirt',
         category : 'Men',
@@ -40,7 +40,7 @@ const data = [
         value : 'Add To Cart'
     },
     {
-        id: 2,
+        id: 6,
         img : 'https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1662130112_1367320.jpg?w=480&dpr=1.3',
         title : 'The Roadster Style Company',
         category : 'Men',
@@ -48,13 +48,68 @@ const data = [
         value : 'Add To Cart'
     },
     {
-        id: 1,
+        id: 7,
         img : 'https://m.media-amazon.com/images/I/61o7dUNtdsL._UY606_.jpg',
         title : 'Men Half T-Shirt',
         category : 'Men',
         price : 4000,
         value : 'Add To Cart'
-    }
+    },
+    
+    {
+        id: 8,
+        img : 'https://rukminim1.flixcart.com/image/832/832/l1l1rww0/watch/5/w/u/1-black-dial-digital-watch-for-boys-black-sports-watch-men-original-imagd4yzszbgba39.jpeg?q=70',
+        title : 'Watch',
+        category : 'Men',
+        price : 4000,
+        value : 'Add To Cart'
+    },
+
+    {
+        id: 9,
+        img : 'https://i02.appmifile.com/306_operator_in/28/01/2021/b86eaaff907d7564077d830b053a15fb!800x800!85.png',
+        title : 'MI 42inches TV',
+        category : 'Electronics',
+        price : 40000,
+        value : 'Add To Cart'
+    },
+
+    {
+        id: 10,
+        img : 'https://m.media-amazon.com/images/I/61o7dUNtdsL._UY606_.jpg',
+        title : 'Men Half T-Shirt',
+        category : 'Men',
+        price : 4000,
+        value : 'Add To Cart'
+    },
+
+    {
+        id: 11,
+        img : 'https://images.samsung.com/is/image/samsung/p6pim/in/sm-p613nzaeinu/gallery/in-galaxy-tab-s6-lite-p610-428695-428695-sm-p613nzaeinu-534744419?$2052_1641_PNG$',
+        title : 'Vivo Tablet ',
+        category : 'Electronics',
+        price : 14000,
+        value : 'Add To Cart'
+    },
+
+    {
+        id: 12,
+        img : 'https://m.media-amazon.com/images/I/81LMvUUqQ3L._UL1500_.jpg',
+        title : '24 carret Gold',
+        category : 'Jwellery',
+        price : 400000,
+        value : 'Add To Cart'
+    },
+
+    {
+        id: 13,
+        img : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRkwSYImlwZtGoIzUHZXV1R2cWqW8ev_iMrA&usqp=CAU',
+        title : 'Clothing',
+        category : 'Women',
+        price : 4000,
+        value : 'Add To Cart'
+    },
+
 
 ];
 
